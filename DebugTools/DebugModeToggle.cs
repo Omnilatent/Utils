37d49508d003945fb4a5b607f191220c83d66558
nguyentrong101094 <nguyentrong101094@gmail.com>
2021-11-03 11:01:25 +0700

    add existing scripts

---
+++ DebugTools/DebugModeToggle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Omnilatent.Utils
+{
+    public class DebugModeToggle : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            GetComponent<Button>().onClick.AddListener(ToggleDebugMode);
+        }
+
+        public void ToggleDebugMode()
+        {
+            DebugManager.forceDebugMode = !DebugManager.forceDebugMode;
+        }
+    }
+}
---
+++ DebugTools/ShowDirectInfo/ShowDirectInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Gront
+{
+    public class ShowDirectInfo : MonoBehaviour
+    {
+        [SerializeField] Canvas m_Canvas;
+        [SerializeField] TextMeshProUGUI m_Text;
+        [SerializeField] Canvas canvasPrefab;
+        string currentText;
+
+        private void Awake()
+        {
+            if (m_Canvas == null)
+            {
+                m_Canvas = Instantiate(canvasPrefab, transform);
+            }
+            if (m_Text == null) { m_Text = GetComponentInChildren<TextMeshProUGUI>(); }
+        }
+
+        public void SetText<T>(T obj)
+        {
+            SetText(obj.ToString());
+        }
+
+        public void SetText(string str)
+        {
+            if (!string.Equals(currentText, str))
+                m_Text.text = currentText = str;
+        }
+    }
+}
