25dbd4c2fb3a8aae2f820653aa8b9ef66847e40c
nguyentrong101094 <nguyentrong101094@gmail.com>
2022-03-22 13:56:28 +0700

    use debug mode flag instead of modes to toggle debug object

--- DebugTools/DebugSwitchActive.cs
+++ DebugTools/DebugSwitchActive.cs
@@ -7,13 +7,18 @@ namespace Omnilatent.Utils
 {
     public class DebugSwitchActive : MonoBehaviour
     {
+        [System.Obsolete("Use DebugModeFlag instead.")]
         public enum Modes
         {
             ShowOnlyInDebugMode = 0,
             ShowInDebugBuild = 2
         }
 
-        public Modes mode;
+        public DebugModeFlag activateCondition = DebugModeFlag.NotSet; //todo: use this instead of Modes
+
+        [Tooltip("mode is deprecated. Use activateCondition instead.")]
+        [System.Obsolete("Use activateCondition instead.")]
+        [SerializeField] private Modes mode;
 
         void Awake()
         {
@@ -22,26 +27,21 @@ namespace Omnilatent.Utils
 
         public bool CheckCanShow()
         {
-            bool canShow = true;
-            switch (mode)
-            {
-                default:
-                case Modes.ShowOnlyInDebugMode:
-                    canShow = DebugManager.IsDebugMode();
-                    break;
-                case Modes.ShowInDebugBuild:
-                    canShow = Debug.isDebugBuild;
-                    break;
-            }
+            bool canShow = DebugManager.CheckDebugFlag(activateCondition);
             gameObject.SetActive(canShow);
             return canShow;
         }
 
         private void OnValidate()
         {
-            if (!Enum.IsDefined(typeof(Modes), mode))
+            /*if (!Enum.IsDefined(typeof(Modes), mode))
             {
                 Debug.LogError($"Field [mode] of {gameObject.name} has invalid value [{mode}]");
+            }*/
+
+            if (activateCondition == DebugModeFlag.NotSet)
+            {
+                Debug.LogError($"Field [activateCondition] of {gameObject.name} need to be set value.");
             }
         }
     }
